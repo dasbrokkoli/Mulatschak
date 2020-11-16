cfe1b43eeb60866b73dbc745c663620061bb93a4
Schwarz <jschwarz@student.tgm.ac.at>
2020-11-16 14:25:57 +0100

    Playground merged

--- app/src/main/java/itp/project/mulatschak/Mulatschakround.java
+++ app/src/main/java/itp/project/mulatschak/Mulatschakround.java
@@ -1,25 +1,11 @@
 package itp.project.mulatschak;
 
-import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
-
-public class Mulatschakround extends AppCompatActivity {
-
-    public static Colors adout;
-    public static Difficulty difficulty;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_mulatschakround);
-
-    }
-
+public class Mulatschakround {
     public static Colors getAdout() {
-        return adout;
+        return null;
     }
 
-    public static Difficulty getDifficulty(){
-        return difficulty;
+    public static Difficulty getDifficulty() {
+        return null;
     }
 }
