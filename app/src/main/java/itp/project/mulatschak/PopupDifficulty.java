279ef0b5b765d1245f3a69c7419d778b4b9f68c3
Mira <mhaselberger@student.tgm.ac.at>
2020-11-16 15:58:40 +0100

    PopUp Difficulty

---
+++ app/src/main/java/itp/project/mulatschak/PopupDifficulty.java
@@ -0,0 +1,13 @@
+package itp.project.mulatschak;
+
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class PopupDifficulty extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.popup_difficulty);
+    }
+}
