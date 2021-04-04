667054193a33e2a3fecb533bf52ab7fa0e7039dc
mheinzel <mheinzel@student.tgm.ac.at>
2021-04-04 16:55:36 +0200

    Einstellungen / Eigene Stiche zurück Button

--- app/src/main/java/itp/project/Mulatschak/SettingsActivity.java
+++ app/src/main/java/itp/project/Mulatschak/SettingsActivity.java
@@ -2,8 +2,10 @@ package itp.project.Mulatschak;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
+import android.widget.Button;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SettingsActivity extends AppCompatActivity {
@@ -17,8 +19,22 @@ public class SettingsActivity extends AppCompatActivity {
         getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
                 WindowManager.LayoutParams.FLAG_FULLSCREEN);
 
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+        Button zurueck = findViewById(android.R.id.home);
+
         // wechseln zu Design-Activity aus Button
         View design = findViewById(R.id.design_button);
         design.setOnClickListener(v -> startActivity(new Intent(SettingsActivity.this, DesignPackActivity.class)));
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
